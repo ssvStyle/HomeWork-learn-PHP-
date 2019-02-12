a8ad9c5c67face885513c9b59d358defef27cb8d
ssv <ssv.style@gmail.com>
2019-02-13 01:13:37 +0200

    Add function.php: Prepared for work

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
-<?php
-echo 'Helo';
-
+<!DOCTYPE html>
+<!---->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Главная</title>
+    </head>
+    <body>
+        <h3>Главная</h3>
+    </body>
+</html>
