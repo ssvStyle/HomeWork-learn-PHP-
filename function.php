a96280f231b878a303492b2505fc8e0f5e89a7a1
ssv <ssv.style@gmail.com>
2019-02-13 21:43:04 +0200

    edits

--- function.php
+++ function.php
@@ -43,11 +43,12 @@ function operationChecking($operation) {
     
 }
 
-function CheckingAllArguments($array) {
+function CheckingAllArguments($array) {//Checking All Arguments
     
     if (!empty($array)) {
         
             return emptyVariableCheck($array['firstNumber'], $array['secondNumber']) && numericCheck($array['firstNumber']) && numericCheck($array['secondNumber']) && operationChecking($array['operation']);
+            
     }
     
 }
