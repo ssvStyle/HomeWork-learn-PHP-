3a8f97d35dc26b625ddfeb4a259d35f6ae9a0cfc
ssv <ssv.style@gmail.com>
2019-02-13 19:52:03 +0200

    working version

--- function.php
+++ function.php
@@ -1,26 +1,50 @@
 <?php
 
           
-function A_plus_B($a, $b) {
+function A_plus_B($a, $b) {//A plus B
     
-    return $a + $b;
-    
-}
-
-function A_minus_B($a, $b) {
-    
-    return $a - $b;
+    return $a . ' + ' . $b . ' = ' . ($a + $b);
     
 }
 
-function A_multiplyBy_B($a, $b) {
+function A_minus_B($a, $b) {//A minus B
     
-    return $a * $b;
+    return $a . ' - ' . $b . ' = ' . ($a - $b);
     
 }
 
-function A_divideBy_B($a, $b) {
+function A_multiplyBy_B($a, $b) { //A multiply by B
     
-    return $a / $b;
+    return $a . ' * ' . $b . ' = ' . ($a * $b);
+    
+}
+
+function A_divideBy_B($a, $b) { //A divide by B
+    
+    return $a . ' / ' . $b . ' = ' . ($a / $b);
+    
+}
+
+function emptyVariableCheck($a, $b) {
+    
+   return !empty($a) && !empty($b);
+    
+}
+
+function numericCheck($variable) {
+    
+    return is_numeric($variable);
+    
+}
+
+function operationChecking($operation) {
+    
+    return !empty($operation);
+    
+}
+
+function CheckingAllArguments() {
+    
+    return variableChecking($a, $b) && operationChecking($operation);
     
 }
--- index.php
+++ index.php
@@ -7,21 +7,21 @@
         <style>
             .container {
                 border: solid 3px #004d00;
-                width: 118px;
-                height: 300px;
+                width: 128px;
+                height: 350px;
                 background: #e6ffe6;
                 padding: 20px;
             }
             #inputSize{
                 width: 100px;
             }
+            .warning{
+                color: red;
+            }
         </style>
     </head>
         <?php include_once __DIR__ . '/function.php';?>
     <body>
-        <pre>
-        <?php var_dump($_GET); ?>
-        </pre>
         
         <h3>Главная</h3>
         
@@ -31,10 +31,20 @@
             
                 <form action="" method="GET">
 
-                    <input type="text" name="firstNumber" id="inputSize" size="2"><br><input type="radio" name="operation" value="A_plus_B">+<br><input type="radio" name="operation" value="A_minus_B">-<br><input type="radio" name="operation" value="A_multiplyBy_B">*<br><input type="radio" name="operation" value="A_divideBy_B">/<br><input type="text" name="secondNumber" id="inputSize" size="2"><br>
-
-                    <br><button type="submit">=</button><br><br><b>Результат: <?php echo $_GET['operation']($_GET['firstNumber'], $_GET['secondNumber']); ?></b>
-
+                    A: <input type="text" name="firstNumber" id="inputSize" size="2"><br>
+                    <input type="radio" name="operation" value="A_plus_B" checked="checked">+<br>
+                            <input type="radio" name="operation" value="A_minus_B">-<br>
+                            <input type="radio" name="operation" value="A_multiplyBy_B">*<br>
+                            <input type="radio" name="operation" value="A_divideBy_B">/<br>
+                    B: <input type="text" name="secondNumber" id="inputSize" size="2"><br>
+                    
+                    
+                    <br><button type="submit">=</button><br><br><b>Результат:</b><br><br>
+                         <?php if (emptyVariableCheck($_GET['firstNumber'], $_GET['secondNumber']) && operationChecking($_GET['operation']) && numericCheck($_GET['firstNumber']) && numericCheck($_GET['secondNumber'])) {
+                                echo $_GET['operation']($_GET['firstNumber'], $_GET['secondNumber']);
+                         } else { ?>
+                    <b class="warning">Ошибка:<br>Пустые поля, текст или не отмеченна операция!!!</b>
+                         <?php } ?>
                 </form>
             
             </div>
