1edd77bc7ff79f623c0a145d3f33023021086c41
ssv <ssv.style@gmail.com>
2019-02-14 00:35:54 +0200

    add bootstrap style: add nav menu: add img folder

---
+++ image.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!---->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Большое изображение</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+    </head>
+        <?php include_once __DIR__ . '/function.php';?>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+              <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                    <a class="nav-link" href="index.php">Главная<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                  <a class="nav-link" href="#!">Гостевая<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link disabled" href="#!">Disabled</a>
+                </li>
+              </ul>
+            </div>
+        </nav>
+        
+        <!-- ^^^^Menu^^^^^-->
+        
+        <div class="nav justify-content-center">
+        <h4>Большое изображение</h4>
+        </div>
+    </body>
+    
+</html>
+
--- index.php
+++ index.php
@@ -4,7 +4,8 @@
     <head>
         <meta charset="UTF-8">
         <title>Главная</title>
-        <style>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
+        <!--<style>
             .container {
                 border: solid 3px #004d00;
                 width: 128px;
@@ -18,40 +19,76 @@
             .warning{
                 color: red;
             }
-        </style>
+        </style>-->
     </head>
         <?php include_once __DIR__ . '/function.php';?>
     <body>
         
-        <h3>Главная</h3>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+              <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item active">
+                  <a class="nav-link" href="#!">Главная<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item active">
+                  <a class="nav-link" href="#!">Гостевая книга<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link disabled" href="#!">Disabled</a>
+                </li>
+              </ul>
+            </div>
+        </nav>
         
         <div class="container">
+            <div class="row">
+                <div class="col">
             
-            <h3>Калькулятор</h3>
+            <h4 class="text-center">Калькулятор</h4>
             
                 <form action="" method="GET">
 
-                    A: <input type="text" name="firstNumber" id="inputSize" size="2"><br>
-                            <input type="radio" name="operation" value="A_plus_B" >+<br>
-                            <input type="radio" name="operation" value="A_minus_B">-<br>
-                            <input type="radio" name="operation" value="A_multiplyBy_B">*<br>
-                            <input type="radio" name="operation" value="A_divideBy_B">/<br>
-                    B: <input type="text" name="secondNumber" id="inputSize" size="2"><br>
+                    <input type="text" name="firstNumber" class="form-control" placeholder="A: ">
+                    <div class="custom-control custom-radio">
+                            <input type="radio" id="customRadio1" name="operation" value="A_plus_B" class="custom-control-input">
+                            <label class="custom-control-label" for="customRadio1">+</label>
+                    </div>
+                    <div class="custom-control custom-radio">
+                            <input type="radio" id="customRadio2" name="operation" value="A_minus_B"  class="custom-control-input">
+                            <label class="custom-control-label" for="customRadio2">-</label>
+                    </div>
+                    <div class="custom-control custom-radio">
+                            <input type="radio" id="customRadio3" name="operation" value="A_multiplyBy_B"  class="custom-control-input">
+                            <label class="custom-control-label" for="customRadio3">*</label>
+                    </div>
+                    <div class="custom-control custom-radio">
+                            <input type="radio" id="customRadio4" name="operation" value="A_divideBy_B"  class="custom-control-input">
+                            <label class="custom-control-label" for="customRadio4">/</label>
+                    </div>
+                   
+                    <input type="text" name="secondNumber" id="inputSize" size="2" class="form-control" placeholder="B: "><br>
                     
                     
-                    <br><button type="submit" value="send">=</button><br><br><b>Результат:</b><br><br>
+                    <button type="submit" class="btn btn-outline-info">=</button><br><br>
                     
-                         <?php if (CheckingAllArguments($_GET)) {
+                         <?php if (CheckingAllArguments($_GET)) { ?>
                              
-                                echo $_GET['operation']($_GET['firstNumber'], $_GET['secondNumber']);
+                    <div class="alert alert-success" role="alert"> <h4>Результат: </h4><?php echo $_GET['operation']($_GET['firstNumber'], $_GET['secondNumber']); ?> </div>
                                 
-                             } elseif (CheckingAllArguments($_GET) || !empty($_GET)) { ?>
+                            <?php } elseif (CheckingAllArguments($_GET) || !empty($_GET)) { ?>
                     
-                    <b class="warning">Ошибка: <br> Пустые строки, текст или не выбрана операция!!!</b>
+                    <div class="alert alert-warning" role="alert">Ошибка: <br> Пустые строки, текст или не выбрана операция!!</div>
                     
                              <?php } ?>
                 </form>
-            
+                    </div>
+                    <div class="col">
+                            <h4 class="text-center">Галерея</h4>
+                    </div>
+                </div>
             </div>
         <hr>
     </body>
