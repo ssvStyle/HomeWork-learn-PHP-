4c9d64df9fc80449c4724c3c28a52790888267a1
ssv <ssv.style@gmail.com>
2019-02-13 15:57:15 +0200

    add form and style

--- index.php
+++ index.php
@@ -4,8 +4,27 @@
     <head>
         <meta charset="UTF-8">
         <title>Главная</title>
+        <style>
+            .container {
+                border: solid 3px #004d00;
+                width: 118px;
+                height: 300px;
+                background: #e6ffe6;
+                padding: 20px;
+            }
+            #inputSize{
+                width: 100px;
+            }
+        </style>
     </head>
     <body>
         <h3>Главная</h3>
+        <div class="container">
+            <h3>Калькулятор</h3>
+            <form action="" method="GET">
+                <input type="number" name="firstNumber" id="inputSize" size="2"><br><input type="radio" name="firstNumber">+<br><input type="radio" name="firstNumber">-<br><input type="radio" name="firstNumber">*<br><input type="radio" name="firstNumber">/<br><input type="number" name="firstNumber" id="inputSize" size="2"><br>
+                <br><button type="submit">=</button>
+            </form>
+            </div>
     </body>
 </html>
