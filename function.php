236690507078351d76cbf475e4bd3976b42e8dcd
ssv <ssv.style@gmail.com>
2019-02-13 16:22:17 +0200

    Added operations functions

--- function.php
+++ function.php
@@ -1,1 +1,26 @@
 <?php
+
+          
+function A_plus_B($a, $b) {
+    
+    return $a + $b;
+    
+}
+
+function A_minus_B($a, $b) {
+    
+    return $a - $b;
+    
+}
+
+function A_multiplyBy_B($a, $b) {
+    
+    return $a * $b;
+    
+}
+
+function A_divideBy_B($a, $b) {
+    
+    return $a / $b;
+    
+}
--- index.php
+++ index.php
@@ -17,14 +17,28 @@
             }
         </style>
     </head>
+        <?php include_once __DIR__ . '/function.php';?>
     <body>
+        <pre>
+        <?php var_dump($_GET); ?>
+        </pre>
+        
         <h3>Главная</h3>
+        
         <div class="container">
+            
             <h3>Калькулятор</h3>
-            <form action="" method="GET">
-                <input type="number" name="firstNumber" id="inputSize" size="2"><br><input type="radio" name="firstNumber">+<br><input type="radio" name="firstNumber">-<br><input type="radio" name="firstNumber">*<br><input type="radio" name="firstNumber">/<br><input type="number" name="firstNumber" id="inputSize" size="2"><br>
-                <br><button type="submit">=</button>
-            </form>
+            
+                <form action="" method="GET">
+
+                    <input type="text" name="firstNumber" id="inputSize" size="2"><br><input type="radio" name="operation" value="A_plus_B">+<br><input type="radio" name="operation" value="A_minus_B">-<br><input type="radio" name="operation" value="A_multiplyBy_B">*<br><input type="radio" name="operation" value="A_divideBy_B">/<br><input type="text" name="secondNumber" id="inputSize" size="2"><br>
+
+                    <br><button type="submit">=</button><br><br><b>Результат: <?php echo $_GET['operation']($_GET['firstNumber'], $_GET['secondNumber']); ?></b>
+
+                </form>
+            
             </div>
+        
     </body>
+    
 </html>
