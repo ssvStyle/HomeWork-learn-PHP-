5d575dbb363c9be6f8aa9f19c4f717645ef30e3f
ssv <ssv.style@gmail.com>
2019-02-17 01:38:18 +0200

    everything works correctly

--- GuestBook.php
+++ GuestBook.php
@@ -13,15 +13,16 @@
         <div class="conteiner">
             
             <div class="row justify-content-center">
+                
                 <div class="col-md-12 offset-6">
-                <?php
-                
-                
-                foreach (getAllNotesFromGuestbookDB() as $note) { ?>
+                    
+                <!--************Note block loop start************************************************-->
+                    
+                <?php foreach (getAllNotesFromGuestbookDB() as $note) { ?>
                     
                     <div class="media col-md-6 border box my-1 pl-0 p-1">
 
-                        <img class="d-flex align-self-center mr-3 img-thumbnail" width="70px" src="/HomeWork/img/no-user-image.gif" alt="image">
+                            <img class="d-flex align-self-center mr-3 img-thumbnail" width="70px" src="/HomeWork/img/no-user-image.gif" alt="image">
                         <div class="media-body">
                             
                             <div class="row"><h5 class="col flex-item-md-between mt-0 pt-0 text-info">Гость</h5><p class="col text-secondary flex-item-md-between text-right mt-0 pt-0 mb-0">15.02.2019</p></div>
@@ -33,8 +34,8 @@
                     </div>
  
                     <?php } ?>
+                <!--************Note block loop end************************************************-->
                 </div>
-                
                 
                 
                 <div class="row col-md-6 pl-0 pr-0">
@@ -42,18 +43,18 @@
                     <form action="GuestBook.php" method="POST" class="col-md-6">
                         
                     <div class="form-group col-md-6 pl-0">
-                    <label for="formGroupExampleInput">Ваше имя</label>
-                    <input type="text" class="form-control" id="formGroupExampleInput" placeholder="You name" name="name">
+                        <label for="formGroupExampleInput">Ваше имя</label>
+                        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="You name" name="name">
                     </div>
                         
                     <div class="form-group col-md-6 pl-0">
-                    <label for="inputEmail" class="sr-only">Email</label>
-                    <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="" name="email">
+                        <label for="inputEmail" class="sr-only">Email</label>
+                        <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="" name="email">
                     </div>
                         
                     <div class="form-group">
-                    <label for="exampleFormControlTextarea1">Ваше сообщение</label>
-                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="text"></textarea>
+                        <label for="exampleFormControlTextarea1">Ваше сообщение</label>
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="text"></textarea>
                     </div>
                     
                   
@@ -62,38 +63,27 @@
                   
                 </form>
                     
+                    <!--************Result (Add note) block start************************************************-->
+                    
                     <?php 
-                    /*
-
-                 *  1. Принять новый msj здесь же
-                 *  2. Обработать на этой же странице
-                 *  3. Убрать отправку ошибки через GET
-                 *  4. Добавить фун-ю которя возвращает ошибку
-                 * 
-                 * 
-                 * 
-                 * 
-                 * 
-                 * 
-                 * 
-                 * 
-                 * 
-                 * 
-                 *                  */
-                    $newNote = !empty($_POST['text']) ? htmlspecialchars(trim($_POST['text'])) : '';
-                    $result = isset($_GET['result']) ? $_GET['result'] : '';
+                        $newNote = !empty($_POST['text']) ? htmlspecialchars(trim($_POST['text'])) : '';
+                        $result = isset($_GET['result']) ? $_GET['result'] : '';
                     
                     if (lineLength($newNote)) { addNewNoteTooGuestbookDB($newNote) ?>
                     
-                        
                         <meta http-equiv="refresh" content="0; url=GuestBook.php?result=ok" />
                     
-                    <?php } elseif (isset($_POST['text']) || $newNote == '') { ?>
-     
+                    <?php } elseif (isset($_POST['text']) || lineLength($newNote)) { ?>
+                        
                         <meta http-equiv="refresh" content="0; url=GuestBook.php?result=error" />
                     
                     <?php } ?>
                     
+                    <!--************Result (Add note) block end************************************************-->
+                    
+                    <!--************Add note error block start************************************************-->
+                        
+                        
                     <?php if ($result == 'ok') { ?>
                         
                         <div class="row col-md-6 alert alert-success h-25 mx-0 justify-content-center" role="alert">Запись добавленна))</div>
@@ -104,11 +94,17 @@
                     
                     <?php } ?>
                     
+                        
+                        <!--************Add note error block end************************************************-->
+                        
                 </div>
-                </div>
+                
             </div>
+            
+        </div>
         
         <hr>
+        
     </body>
     
 </html>
